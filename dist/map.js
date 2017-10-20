4693cfd356f58bc297925f1e47b8e0d2aca4e9fb
Yuriy Kurenkov <mr.kurenkov@gmail.com>
2017-10-20 19:00:32 +0200

    Map coordinates fixed

--- dist/map.js
+++ dist/map.js
@@ -2,7 +2,7 @@
 function initMap() {
     var map = new google.maps.Map(document.getElementById("map"), {
         zoom: 17,
-        center: {lat: 45.0198446, lng: 38.9720053},
+        center: {lat: 45.01985, lng: 38.9742},
         disableDefaultUI:true,
         scrollwheel: false,
         zoomControl: true,
@@ -13,14 +13,14 @@ function initMap() {
         path: "M27.4,3.9C24.6,1.4,20.6,0,16.3,0S8,1.4,5.2,3.9C1.8,7,0,11.6,0,17.3c0,12.4,14.3,26.9,14.9,27.6 c0.4,0.4,0.9,0.6,1.4,0.6c0.5,0,1-0.2,1.4-0.6c0.6-0.6,14.9-15.1,14.9-27.6C32.6,11.6,30.8,7,27.4,3.9z M16.3,26.9 C10.6,26.9,6,22.3,6,16.5S10.6,6.1,16.3,6.1s10.2,4.6,10.2,10.4C26.5,22.2,21.9,26.9,16.3,26.9z",
         fillColor: "#a97140",
         fillOpacity: 1,
-        anchor: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(24, 40),
         strokeWeight: 0,
         scale: 1
-    }
+    };
 
     var marker = new google.maps.Marker({
         map: map,
-        position: {lat: 45.0198446, lng: 38.9720053},
+        position: {lat: 45.01985, lng: 38.9742},
         draggable: false,
         // icon: "./img/map-marker.png"
         icon: icon

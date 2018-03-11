005154fd3fc6e36171e5a33285367f205d4e4b29
Yuriy Kurenkov <mr.kurenkov@gmail.com>
2018-03-11 19:30:22 +0200

    Add roistat counter

--- wp/page-home.php
+++ wp/page-home.php
@@ -81,6 +81,20 @@
 
 <body>
     <!-- TODO: google counter -->
+    <script>
+        (function(w, d, s, h, id) {
+            w.roistatProjectId = id;
+            w.roistatHost = h;
+            var p = d.location.protocol == "https:" ? "https://" : "http://";
+            var u = /^.*roistat_visit=[^;]+(.*)?$/.test(d.cookie) ? "/dist/module.js" : "/api/site/1.0/"+id+"/init";
+            var js = d.createElement(s);
+            js.charset="UTF-8";
+            js.async = 1;
+            js.src = p+h+u;
+            var js2 = d.getElementsByTagName(s)[0];
+            js2.parentNode.insertBefore(js, js2);
+        })(window, document, 'script', 'cloud.roistat.com', 'dd06ef1b09acdaf925a93997606ae743');
+    </script>
 
     <!-- Header -->
     <header class="page-header">
@@ -214,6 +228,8 @@
                 </div>
             </blockquote>
         </div>
+
+
 
         <div class="hero__deco"></div>
     </section>
